3f45f51aa99e84f7621cdf1a341daa5985a19eaa
cowqun <eminovcowqun96@gmail.com>
2024-05-01 14:37:39 +0400

    Add project files.

---
+++ MyStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp96
+{
+     public class MyStack<T>:IEnumerable
+    {
+            public T[] _arr;
+            public int Count { get; private set; }
+            public int Capacity { get; private set; }
+
+
+            public MyStack()
+            {
+                Capacity = 4;
+                Count = 0;
+                _arr = new T[Capacity];
+            }
+            public MyStack(int capacity)
+            {
+                Capacity = capacity;
+                _arr = new T[capacity];
+            }
+           
+            public void Push(T item)
+            {
+                if (Count == Capacity)
+                {
+                    Capacity *= 2;
+                    Array.Resize(ref _arr, Capacity);
+                }
+                _arr[Count++] = item;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    yield return _arr[i];
+                }
+            }
+            public void Pop()
+            {
+                Count--;
+                if (Count <= 0)
+                {
+                    Console.WriteLine("There is no element to delete");
+                }
+                else
+                {
+                    Array.Resize(ref _arr, Count);
+                }
+
+            }
+            public T Peek()
+            {
+                T lastValue = _arr[Count - 1];
+                return lastValue;
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp96
+{
+      public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("----");
+            Console.WriteLine(stack.Capacity);
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
